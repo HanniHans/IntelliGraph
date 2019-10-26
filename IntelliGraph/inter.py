bef9aae9e8c70636bfd97ec8556b418fbdc2e249
HanniHans <HanniHans@github.com>
2019-10-26 18:54:09 -0500

    .

--- IntelliGraph/inter.py
+++ IntelliGraph/inter.py
@@ -1,8 +1,43 @@
-from tkinter import *
-raiz=Tk()
-raiz.title("Venta de pruebas")
-raiz.resizable(0,0)
+#from tkinter import *
+#raiz=Tk()
+#raiz.title("Venta de pruebas")
+#raiz.resizable(0,0)
 #raiz.iconbitmap()
-raiz.geometry("750x400")
-#raiz.config(bg=white)
-raiz.mainloop()
+#raiz.geometry("750x400")
+#raiz.config(bg="white")
+#raiz.mainloop()
+
+
+#mi frame.pack()
+
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+
+#class 
+
+class karl( Frame ):
+    def __init__( self ):
+        tk.Frame.__init__(self)
+        self.pack()
+        self.master.title("Karlos")
+        self.button1 = Button( self, text = "CLICK HERE", width = 25,
+                               command = self.new_window )
+        self.button1.grid( row = 0, column = 1, columnspan = 2, sticky = W+E+N+S )
+    def new_window(self):
+        self.newWindow = karl2()
+class karl2(Frame):     
+    def __init__(self):
+        new =tk.Frame.__init__(self)
+        new = Toplevel(self)
+        new.title("karlos More Window")
+        new.button = tk.Button(  text = "PRESS TO CLOSE", width = 25,
+                                 command = self.close_window )
+        new.button.pack()
+    def close_window(self):
+        self.destroy()
+def main(): 
+    karl().mainloop()
+if __name__ == '__main__':
+    main()
